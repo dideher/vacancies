e5daa1cd82b8d5bae0886c911473a4c9b0ca3500
Filip Slavik <filip@pressidium.com>
2021-01-27 13:55:24 +0200

    added migrations and heroku files

--- vacancies/settings.py
+++ vacancies/settings.py
@@ -140,3 +140,8 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'no.reply.dde.irakleiou@gmail.com'
 EMAIL_HOST_PASSWORD = 'Dde#2020@iraK'
+
+
+# Configure Django App for Heroku.
+import django_heroku
+django_heroku.settings(locals())
