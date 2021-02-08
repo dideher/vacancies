addb5bf3d5cb66bd7864b1edfb3245629a4d0761
Filip Slavik <filip@pressidium.com>
2021-02-08 13:16:22 +0200

    added sendgrind configuration in heroku settings

--- app/vacancies/settings/heroku.py
+++ app/vacancies/settings/heroku.py
@@ -5,6 +5,15 @@ DEBUG = False
 
 SECRET_KEY = os.environ['SECRET_KEY']
 
+# Email settings
+SENDGRID_API_KEY = os.getenv('SENDGRID_API_KEY')
+
+EMAIL_HOST = 'smtp.sendgrid.net'
+EMAIL_HOST_USER = 'apikey' # this is exactly the value 'apikey'
+EMAIL_HOST_PASSWORD = SENDGRID_API_KEY
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
