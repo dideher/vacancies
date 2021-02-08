286bdb288c931ed4e533906de86a82ba833c0bb7
Filip Slavik <filip@pressidium.com>
2021-02-08 13:57:55 +0200

    added EMAIL_SUBJECT_PREFIX setting in heroku settings

--- app/vacancies/settings/heroku.py
+++ app/vacancies/settings/heroku.py
@@ -7,7 +7,8 @@ SECRET_KEY = os.environ['SECRET_KEY']
 
 # Email settings
 SENDGRID_API_KEY = os.getenv('SENDGRID_API_KEY')
-DEFAULT_FROM_EMAIL = 'kena@dideira.gr'
+DEFAULT_FROM_EMAIL = 'no-reply@dideira.gr'
+EMAIL_SUBJECT_PREFIX = '[kena_staging] '
 
 EMAIL_HOST = 'smtp.sendgrid.net'
 EMAIL_HOST_USER = 'apikey' # this is exactly the value 'apikey'
