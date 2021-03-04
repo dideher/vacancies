d799faac29ae46688b87ca938324a55e9fa8d367
Filip Slavik <filippos@slavik.gr>
2021-03-04 15:04:53 +0200

    added entry variants for musical schools

--- app/main_app/forms.py
+++ app/main_app/forms.py
@@ -1,8 +1,6 @@
-from typing import Tuple
 from django import forms
 from django.utils.translation import gettext_lazy as _
 from .models import Entry, EntryVariantType
-from main_app.models import Specialty
 
 VALID_ENTRY_VARIANTS_DEFAULT = (
     EntryVariantType.GENERAL_EDUCATION ,
@@ -21,8 +19,50 @@ VALID_ENTRY_VARIANTS_DEFAULT = (
 )
 
 VALID_ENTRY_VARIANTS_MUSIC_SCHOOL = (
-    EntryVariantType.MUSICAL_SOME_TYPE,
+    EntryVariantType.MUSICAL_AKORTEON,
+    EntryVariantType.MUSICAL_BIOLA,
+    EntryVariantType.MUSICAL_BIOLI,
+    EntryVariantType.MUSICAL_BIOLI_TRADITIONAL,
+    EntryVariantType.MUSICAL_BIOLOTSELO,
+    EntryVariantType.MUSICAL_GAINDA,
+    EntryVariantType.MUSICAL_DIEYTH_ORXISTRAS,
+    EntryVariantType.MUSICAL_DIEYTH_XORODIAS,
+    EntryVariantType.MUSICAL_ZOURNAS,
+    EntryVariantType.MUSICAL_THEORHTIKA_BYZ_MOUSIKHS,
+    EntryVariantType.MUSICAL_THEORHTIKA_EYR_MOUSIKHS,
+    EntryVariantType.MUSICAL_KANONAKI,
+    EntryVariantType.MUSICAL_KITHARA_HLEKTRIKI,
+    EntryVariantType.MUSICAL_KITHARA_KLASIKI,
+    EntryVariantType.MUSICAL_KLARINETO,
+    EntryVariantType.MUSICAL_KLARINO_PARADOSIAKO,
+    EntryVariantType.MUSICAL_KONTRAMPASO,
+    EntryVariantType.MUSICAL_KORNO,
+    EntryVariantType.MUSICAL_KROUSTA_EYR,
+    EntryVariantType.MUSICAL_KROUSTA_PARADOSIAKA,
+    EntryVariantType.MUSICAL_LAOUTO,
+    EntryVariantType.MUSICAL_LYRA_DODEKANHSOU,
+    EntryVariantType.MUSICAL_LYRA_KRHTIKH,
+    EntryVariantType.MUSICAL_LYRA_MAKEDONIAS,
+    EntryVariantType.MUSICAL_LYRA_POLITIKH,
+    EntryVariantType.MUSICAL_LYRA_PONTIAKH,
+    EntryVariantType.MUSICAL_MANTOLINO,
+    EntryVariantType.MUSICAL_MOUSIKHS_TEXNLOGIAS,
+    EntryVariantType.MUSICAL_MPASO_HLEKTRIKO,
+    EntryVariantType.MUSICAL_MPOUZOUKI_TRIXORDO,
+    EntryVariantType.MUSICAL_NEI_KABALI_PAR_AYLOI,
+    EntryVariantType.MUSICAL_OMPOE,
+    EntryVariantType.MUSICAL_OUTI,
+    EntryVariantType.MUSICAL_PIANO,
+    EntryVariantType.MUSICAL_SANTOURI,
+    EntryVariantType.MUSICAL_SAXOFONO,
+    EntryVariantType.MUSICAL_TAMPOURA,
+    EntryVariantType.MUSICAL_TOUMPA,
+    EntryVariantType.MUSICAL_TROMPETA,
+    EntryVariantType.MUSICAL_TROMPONI,
+    EntryVariantType.MUSICAL_FAGOTO,
+    EntryVariantType.MUSICAL_FLAOUTO,
 )
+
 
 def get_variant_choices(**kwargs):
     """
@@ -49,12 +89,14 @@ def get_variant_choices(**kwargs):
         
     return valid_choices
 
+
 class EntryCreateForm(forms.ModelForm):
     # check https://stackoverflow.com/questions/52920107/how-to-populate-fill-choicefield-with-custom-values-in-django-forms
     variant = forms.ChoiceField(
         label=_('Τύπος Κενού / Πλεονάσματος'), 
         help_text=_('Επιλέξετε το είδος του κενού'), 
     )
+
     class Meta:
         model = Entry
         fields = ['specialty', 'type', 'variant', 'hours', 'description']
@@ -87,4 +129,4 @@ class EntryUpdateForm(forms.ModelForm):
             'type': forms.RadioSelect,
             'description': forms.Textarea(attrs={'rows': 3}),
         }
-    
+
