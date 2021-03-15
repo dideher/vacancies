163f38c6cde2b77d26e4bd2e90e081323b970119
Filip Slavik <filippos@slavik.gr>
2021-03-15 08:27:36 +0200

    added school_type, school_timetable and school_variant attributes in school models.py

--- app/schools/models.py
+++ app/schools/models.py
@@ -1,4 +1,25 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+
+class SchoolType(models.TextChoices):
+    GYMNASIO = 'GYMNASIO', _('Γυμνάσιο')
+    GYMNASIO_LYKEIO = 'GYMNASIO_LYKEIO', _('Γυμνάσιο Με Λυκειακές Τάξεις')
+    LYKEIO = 'LYKEIO', _('Λύκειο')
+    EPAL = 'EPAL', _('ΕΠΑΛ'),
+
+
+class SchoolTimetable(models.TextChoices):
+    ESPERINO = 'ESPERINO', _('Εσπερινό')
+    HMERISIO = 'HMERISIO', _('Ημερήσιο')
+
+
+class SchoolVariant(models.TextChoices):
+    GENERAL = 'GENERAL', _('Γενικής Παιδείας')
+    MUSIC = 'MUSIC', _('Μουσικό')
+    ART = 'ART', _('Καλλιτεχνικό')
+    PEIRAMATIKO = 'PEIRAMATIKO', _('Πειραματικό')
+    EIDIKO = 'EIDIKO', _('Ειδικό')
 
 
 class School(models.Model):
@@ -8,6 +29,30 @@ class School(models.Model):
     phone = models.CharField(max_length=15, null=True, verbose_name='Τηλέφωνο Σχολείου')
     address = models.CharField(max_length=100, null=True, verbose_name='Τηλέφωνο Σχολείου')
     connected_to_user = models.BooleanField(default=False, verbose_name='Σύνδεση με χρήστη')
+    school_type = models.CharField(
+        max_length=16,
+        verbose_name=_('Έιδος Σχολείου'),
+        help_text=_('Επιλέξετε τον τύπο της σχολική μονάδας (Γυμνάσιο/Λύκειο/...)'),
+        choices=SchoolType.choices,
+        default=SchoolType.LYKEIO,
+        null=False
+    )
+    school_timetable = models.CharField(
+        max_length=16,
+        verbose_name=_('Ωράριο Λειτουργίας'),
+        help_text=_('Επιλέξετε τον το ωράριο λειτουργίας της σχολικής μονάδας'),
+        choices=SchoolTimetable.choices,
+        default=SchoolTimetable.HMERISIO,
+        null=False
+    )
+    school_variant = models.CharField(
+        max_length=16,
+        verbose_name=_('Κατηγορία'),
+        help_text=_('Επιλέξετε την κατηγορία της σχολικής μονάδας'),
+        choices=SchoolVariant.choices,
+        default=SchoolVariant.GENERAL,
+        null=False
+    )
 
     def __str__(self):
         return f'{self.name}'
