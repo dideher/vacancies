a9556431874e8b622428efb317d57d04b12e320d
Filip Slavik <filippos@slavik.gr>
2022-04-15 14:50:12 +0300

    add support for django-filtering in EntryList API call

--- app/api/urls.py
+++ app/api/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from api.views import SchoolViewSet, EntryViewSet, SpecialtyViewSet, SchoolEntryList, SchoolPendingList
+from api.views import SchoolViewSet, EntryList, SpecialtyViewSet, SchoolEntryList, SchoolPendingList
 
 # Create a router and register our viewsets with it.
 router = DefaultRouter()
 router.register(r'schools', SchoolViewSet)
-router.register(r'entries', EntryViewSet)
+router.register(r'entries', EntryList)
 router.register(r'specialties', SpecialtyViewSet)
 router.register(r'schools/(?P<pk>\d+)/entries', SchoolEntryList, basename='foooo/')
 router.register(r'pending_schools', SchoolPendingList)
