59d9cf744556f4afa332ea2c2beca1d4db8ac8af
Filip Slavik <filippos@slavik.gr>
2022-05-03 23:38:25 +0300

    greatly updating school upload/refresh operation which can now run in update only mode.

--- app/excels/forms.py
+++ app/excels/forms.py
@@ -3,3 +3,6 @@ from django import forms
 
 class UploadFileForm(forms.Form):
     file = forms.FileField(label='Αρχείο')
+    purge_existing_schools = forms.BooleanField(initial=False, label='Λειτουργία Αρχικοποίησης', required=False,
+                                                help_text='Αν επιλέξετε αρχικοποίηση, τότε τυχόν υπαρκτά σχολεία '
+                                                          'στην βάση θα διαγραφούν αμετάκλειτα')
