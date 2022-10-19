198630eaa1a5b1e5306811fa2aea5c55bf8dccad
Filip Slavik <filippos@slavik.gr>
2022-10-19 09:10:28 +0300

    initial version of dockerized variant

--- gunicorn_conf.py
+++ gunicorn_conf.py
@@ -6,8 +6,8 @@ import os
 
 workers_per_core_str = os.getenv("WORKERS_PER_CORE", "2")
 web_concurrency_str = os.getenv("WEB_CONCURRENCY", None)
-host = os.getenv("HOST", "0.0.0.0")
-port = os.getenv("PORT", "80")
+host = os.getenv("HOST", "127.0.0.1")
+port = os.getenv("PORT", "8000")
 bind_env = os.getenv("BIND", None)
 use_loglevel = os.getenv("LOG_LEVEL", "info")
 if bind_env:
