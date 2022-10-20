e2a0c7ff3aa88084b3b29af52f0191f9aed9a6f2
Filip Slavik <filippos@slavik.gr>
2022-10-20 10:28:29 +0300

    added staging settings

---
+++ app/vacancies/settings/staging.py
@@ -0,0 +1,47 @@
+import os
+from vacancies.settings.common import *
+
+DEBUG = True
+
+SECRET_KEY = os.environ['SECRET_KEY']
+
+# Email settings
+DEFAULT_FROM_EMAIL = 'no-reply-staging@dideira.gr'
+EMAIL_SUBJECT_PREFIX = '[kena_staging] '
+
+
+# Database
+# https://docs.djangoproject.com/en/3.1/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASS'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
+        'OPTIONS': {
+            'init_command': 'SET default_storage_engine=INNODB',
+        }
+    }
+}
+
+# Sentry Settings
+try:
+    import sentry_sdk
+    from sentry_sdk.integrations.django import DjangoIntegration
+
+    sentry_sdk.init(
+        dsn=os.getenv('SENTRY_DSN'),
+        integrations=[DjangoIntegration()],
+        traces_sample_rate=1.0,
+        environment=os.getenv('SENTRY_ENVIRONMENT'),
+
+        # If you wish to associate users to errors (assuming you are using
+        # django.contrib.auth) you may enable sending PII data.
+        send_default_pii=True
+    )
+except ImportError:
+    # probably dependecies not installed
+    pass
