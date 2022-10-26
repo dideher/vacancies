38c2a39b2b7f65f10081b6fd55d9d1a1374640da
Filip Slavik <filippos@slavik.gr>
2022-10-26 08:56:55 +0300

    updated docker-dev configuration with correct ALLOWED_HOSTS

--- app/vacancies/settings/docker-dev.py
+++ app/vacancies/settings/docker-dev.py
@@ -9,7 +9,7 @@ SECRET_KEY = 'ze7xy@oo3m#6g-%gs@)@49jvvrxw^&bgagkqby)sc1!kk95j7^'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '0.0.0.0', '127.0.0.1', '192.168.1.4', '192.168.2.160']
+ALLOWED_HOSTS = ['*']
 
 
 # Database
