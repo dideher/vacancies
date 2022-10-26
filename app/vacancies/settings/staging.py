d919b432fe931c0056acb250c55a187a7de47f7d
Filip Slavik <filippos@slavik.gr>
2022-10-26 09:00:04 +0300

    updated configruations

--- app/vacancies/settings/staging.py
+++ app/vacancies/settings/staging.py
@@ -1,14 +1,15 @@
-import os
 from vacancies.settings.common import *
 
-DEBUG = True
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
-ALLOWED_HOSTS = ['localhost', '0.0.0.0', '127.0.0.1', 'kena-new.dideira.gr', 'kena-next.dideira.gr']
+# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ['SECRET_KEY']
 
-# Email settings
-DEFAULT_FROM_EMAIL = 'no-reply-staging@dideira.gr'
-EMAIL_SUBJECT_PREFIX = '[kena_staging] '
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = ['*']
 
 
 # Database
@@ -17,10 +18,10 @@ EMAIL_SUBJECT_PREFIX = '[kena_staging] '
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASS'),
-        'HOST': os.getenv('DB_HOST'),
+        'NAME': os.getenv('DB_NAME', 'db_vacancies'),
+        'USER': os.getenv('DB_USER', 'vacancies_user'),
+        'PASSWORD': os.getenv('DB_PASS', 'vacancies_pass'),
+        'HOST': os.getenv('DB_HOST', 'db'),
         'PORT': os.getenv('DB_PORT', 3306),
         'OPTIONS': {
             'init_command': "SET default_storage_engine=INNODB; SET sql_mode='STRICT_TRANS_TABLES'",
@@ -34,21 +35,3 @@ DATABASES = {
 STATIC_ROOT = '/app/static_files'
 MEDIA_ROOT = '/app/media_files'
 
-# Sentry Settings
-try:
-    import sentry_sdk
-    from sentry_sdk.integrations.django import DjangoIntegration
-
-    sentry_sdk.init(
-        dsn=os.getenv('SENTRY_DSN'),
-        integrations=[DjangoIntegration()],
-        traces_sample_rate=1.0,
-        environment=os.getenv('SENTRY_ENVIRONMENT'),
-
-        # If you wish to associate users to errors (assuming you are using
-        # django.contrib.auth) you may enable sending PII data.
-        send_default_pii=True
-    )
-except ImportError:
-    # probably dependecies not installed
-    pass
