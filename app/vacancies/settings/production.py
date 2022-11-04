d86f1c67d10c1f64ad002f43387e37a6ba7d67f3
Filip Slavik <filippos@slavik.gr>
2022-11-04 08:39:02 +0200

    fixed production settings

--- app/vacancies/settings/production.py
+++ app/vacancies/settings/production.py
@@ -30,6 +30,26 @@ DATABASES = {
     }
 }
 
+
+# Sentry Settings
+try:
+    import sentry_sdk
+    from sentry_sdk.integrations.django import DjangoIntegration
+
+    sentry_sdk.init(
+        dsn=os.getenv('SENTRY_DSN'),
+        integrations=[DjangoIntegration()],
+        traces_sample_rate=1.0,
+        environment=os.getenv('SENTRY_ENVIRONMENT'),
+
+        # If you wish to associate users to errors (assuming you are using
+        # django.contrib.auth) you may enable sending PII data.
+        send_default_pii=True
+    )
+except ImportError:
+    # probably dependencies not installed
+    pass
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 STATIC_ROOT = '/app/static_files'
